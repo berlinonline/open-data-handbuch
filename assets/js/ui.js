43a7885b50037024472d44465018ef2b1f272740
Knud Möller <knud@datalysator.com>
2019-11-07 11:26:44 +0100

    add responsive TOC-menu and burger menu button on left side

---
+++ assets/js/ui.js
@@ -0,0 +1,52 @@
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink'),
+        content  = document.getElementById('main');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    function toggleAll(e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    }
+
+    menuLink.onclick = function (e) {
+        toggleAll(e);
+    };
+
+    content.onclick = function(e) {
+        if (menu.className.indexOf('active') !== -1) {
+            toggleAll(e);
+        }
+    };
+
+    var el = document.getElementsByClassName("bo-menu-item-link");
+    for (let item of el) {
+        item.outerHTML = "<div class='bo-menu-item-wrapper'>" + item.outerHTML + "</div>";
+    }
+    
+
+}(this, this.document));
